438c9848e32f0ca5d7cc0ab3927452fcf69a8f41
Ievgenii Makhonko <yobaua@gmail.com>
2022-07-08 16:00:26 +0800

    commit

---
+++ Item.java
@@ -0,0 +1,44 @@
+public class Item {
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Item(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+public Item(Item sourse){
+        this.name = sourse.name;
+        this.price = sourse.price;
+        this.quantity = sourse.quantity;
+}
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + ": " + this.price +" ("+ this.quantity+")";
+    }
+}
